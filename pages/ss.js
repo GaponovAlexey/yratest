ddb3e5244afc8ce1226a8ebb0b32e9beee101fa4
Kekelberi <alexei.gaponov@gmail.com>
2022-05-27 17:23:55 +0300

    save

---
+++ pages/ss.js
@@ -0,0 +1,54 @@
+export const ss = () => {
+  return (
+    <div>
+      ScorpionsBilly F GibbonsRitchie Blackmore's RainbowGrand Funk
+      RailroadJefferson AirplaneBad Boys BlueJourneyPowerwolfLoveMonster
+      MagnetShocking BlueJimi HendrixRainbowDepeche ModeBonnie TylerLake Of
+      TearsZZ TopSavoy BrownOzzy OsbourneHelloweenSteppenwolfElmore
+      KingTiamatJudas PriestClan of XymoxIron MaidenStrawberry Alarm
+      ClockManowarCreedence Clearwater RevivalGary MooreTen Years AfterGolden
+      EarringParadise LostRichard ThompsonABBAWhitesnakeEuropeAC/DCSmokieProcol
+      HarumElderType O NegativeKyussNocturnal RitesCurtis MayfieldAcceptThe
+      BugglesBoney M.HammerFallDanzigElectric Light OrchestraYoussou N'DourThe
+      Pretty ThingsThe First EditionThe WhoR.E.M.DioMötley CrüeMichael
+      JacksonDef LeppardLove and RocketsUFOBilly IdolCreamEmerson, Lake &
+      PalmerThe CarsSavatageAlphavilleDaft PunkKaleidoscopeKingdom
+      ComedArtagnanThe MonkeesKISSDeep PurpleUriah HeepDolores
+      O'RiordanRöyksoppLed ZeppelinThe SteppesThe ClashTed NugentThe DoorsCock
+      RobinBrocas HelmW.A.S.P.Curved AirBaccaraThin LizzyGraveyard
+      TrainQuartzRammsteinDamageplanIn FlamesSchillerBlack SabbathJoe CockerBig
+      Brother & The Holding CompanyMarilyn MansonCrimson GloryHalfordThe
+      CranberriesMotörheadThe MoveCandlemassBloodboundKen HensleyThe Heavy
+      Eyesa-haThe BeatlesGhostUltimate SpinachThe Guess WhoTwisted SisterREO
+      SpeedwagonFreedom CallPainThemLeatherwolfMercyful FateThe Rolling
+      StonesSteely DanTimothy FleetBattle BeastGrateful DeadMadonnaHerman's
+      HermitsAsiaBandolier KingsThe KinksHaddawayRushTOTOStephanieThe Beach
+      BoysThe Electric PrunesPink FloydLindemannLaura BraniganBon JoviJanis
+      JoplinEric ClaptonDon McLeanCutting CrewRoky EricksonNoel ReddingThe
+      BandThe ShadowsBlue Öyster CultDavid BowieThe
+      YardbirdsDraconianGandalfLoverboyMolly HatchetBloody HammersStatus QuoJam
+      & SpoonSandraThe Moody BluesSurvivorOpethPretty MaidsBilly SquierStingRory
+      GallagherThe CardigansBob DylanSteve Harley & Cockney RebelPrincess
+      ChelseaKansasKylie MinoguePaul EngemannDave StewartThe FugsSammy HagarEyes
+      Wide OpenAmy MacdonaldModern TalkingBauhausThe United States Of
+      AmericaEaglesDschinghis KhanDavid GilmourMidifine SystemsBlackfootEdgar
+      WinterQueens of the Stone AgeStray CatsSt. John GreenFoghatSteve
+      HackettMetal ChurchU.D.O.Vicious RumorsAmericaStan RidgwayJohn
+      NorumConcrete BlondeThe EasybeatsCrematoryBee GeesCanned HeatIced
+      EarthDareScatman JohnStarshipTOOLSleepOmenBlue SystemBlondie石元 丈晴Joe
+      WalshWarriorBruce DickinsonThe Night Flight OrchestraBronski BeatC.C.
+      CatchTenacious DRob ZombieElfMåneskinSaxonHeaven & HellRam JamTheatre Of
+      TragedyCamelBreadAllman Brothers BandIrene CaraJohn Paul JonesGrave
+      DiggerPortugal. The ManLowChris SquireAngel WitchMcAuley Schenker GroupThe
+      ConnellsThe BlanksPentagramThe Gap BandStreetheartWarAutographAlice
+      CooperFleetwood MacBomfunk MC'sThe WeekndAxel Rudi PellNorma Tanega13th
+      Floor ElevatorsEloyMagnumThe AnimalsThe ZombiesPhil CollinsSweetBaha'i
+      Victory ChorusThe ContoursMichael SembelloSisters of MercyThe
+      HootersKrokusTen SharpHenrik PalmScott McKenzieDirkschneider & The Old
+      GangGlenn MedeirosPet Shop BoysHurricaneAl StewartSentencedLa FemmeLynyrd
+      SkynyrdDire StraitsEiffel 65Jean-Luc PontyThe ByrdsPaul McCartneyShark
+      IslandChuck BerryLOUDNESSHarlequinRichard MarxFates WarningA Flock Of
+      SeagullsPaul StanleyStratovariusLittle RichardAnvil
+    </div>
+  )
+}
